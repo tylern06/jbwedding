b402a9036d6b41a267b2427cc7ea8464861d8471
Tyler Nguyen <tynguyen06@gmail.com>
2016-11-28 17:08:54 -0800

    fix email

--- server.js
+++ server.js
@@ -25,22 +25,24 @@ var nodemailer = require('nodemailer');
 var transporter = nodemailer.createTransport('smtps://pokemongomapper%40gmail.com:pikachu1@smtp.gmail.com');
 
 
-app.get('/', function (req,res){
-		console.log(req.body)
- 		res.render("index");
-})
+app.get('/', function (req,res){     console.log(req.body)
+res.render("index"); })
 
 app.post('/rsvp', function (req,res){
+	console.log('rsvp submitted');
+	console.log(req.body)
 	// setup e-mail data with unicode symbols
-		if(req.body.name > 2){
+		if(req.body.name.length > 2){
+			console.log('mail options');
 			var mailOptions = {
-			    from: '"RSVP" <pokemongomapper@bgmail.com>', // sender address
-			    to: 'belindanghiem@gmail.com', // list of receivers
+  				from: '"RSVP" <pokemongomapper@gmail.com>', // sender address 
+			    to: 'tynguyen06@gmail.com', // list of receivers
 			    subject: 'Josh & Belinda Wedding RSVP', // Subject line
 			    text: 'Hey Guys', // plaintext body
-			    html:  "<table style='border-collapse: collapse; border: 1px solid black;width: 250px;'><tr><th style='text-align: left;'>Name</th><th style='text-align: left;'>RSVP</th><th style='text-align: left;'>Song Request</th></tr><tr><td>" + req.body.name + "</td><td>" + req.body.rsvp + "</td><td>" + req.body.song + "</td></tr></table>"// html body
+			    html: "<table style='border: 1px solid black;border-collapse: collapse;width: 100%;'><tr><th style='border: 1px solid black;height: 50px;text-align: left;padding: 15px;'>Name</th><th style='border: 1px solid black;height: 50px;text-align: left;padding: 15px;'>RSVP</th><th style='border: 1px solid black;height: 50px;text-align: left;padding: 15px;'># of Guests</th><th style='border: 1px solid black;height: 50px;text-align: left;padding: 15px;'>Email</th><th style='border: 1px solid black;height: 50px;text-align: left;padding: 15px;'>Address</th><th style='border: 1px solid black;height: 50px;text-align: left;padding: 15px;'>Phone</th><th style='border: 1px solid black;height: 50px;text-align: left;padding: 15px;'>Song Request</th><th style='border: 1px solid black;height: 50px;text-align: left;padding: 15px;'>Comments</th></tr><tr><td style='border: 1px solid black;padding: 15px;text-align: left;'>" + req.body.name + "</td><td style='border: 1px solid black;padding: 15px;text-align: left;'>" + req.body.rsvp + "</td><td style='border: 1px solid black;padding: 15px;text-align: left;'>" + req.body.guests + "</td><td style='border: 1px solid black;padding: 15px;text-align: left;'>" + req.body.email + "</td><td style='border: 1px solid black;padding: 15px;text-align: left;'>" + req.body.address + "</td><td style='border: 1px solid black;padding: 15px;text-align: left;'>" + req.body.phone + "</td><td style='border: 1px solid black;padding: 15px;text-align: left;'>"+ req.body.song + "</td><td style='border: 1px solid black;padding: 15px;text-align: left;'>" + req.body.comments + "</td></tr></table>"
+
+			    // html:  "<table style='border-collapse: collapse; border: 1px solid black;width: 250px;'><tr><th style='text-align: left;border: 1px solid black;width: 250px;'>Name</th><th style='text-align: left;border: 1px solid black;width: 250px;'>RSVP</th><th style='text-align: left;border: 1px solid black;width: 250px;'># of Guests</th><th style='text-align: left;border: 1px solid black;width: 250px;'>Email</th><th style='text-align: left;border: 1px solid black;width: 250px;'>Address</th><th style='text-align: left;border: 1px solid black;width: 250px;'>Phone</th><th style='text-align: left;border: 1px solid black;width: 250px;'>Song Request</th><th style='text-align: left;border: 1px solid black;width: 250px;'>Comments</th></tr><tr><td>" + req.body.name + "</td><td>" + req.body.rsvp + "</td><td>" + req.body.guests + "</td><td>" + req.body.email + "</td><td>" + req.body.address + "</td><td>" + req.body.phone + "</td><td>"+ req.body.song + "</td><td>" + req.body.comments + "</td></tr></table>"// html body
 			};
-			console.log(req.body)
 			//send mail with defined transport object
 			transporter.sendMail(mailOptions, function(error, info){
 			    if(error){
@@ -50,7 +52,7 @@ app.post('/rsvp', function (req,res){
 			});
 		}
 		res.redirect('/');
-})
+});
 
 var port = process.env.PORT || 5000;
 var server = app.listen(port, function(){
